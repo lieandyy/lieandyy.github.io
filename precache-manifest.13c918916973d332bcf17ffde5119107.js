c0619adf48238c26b5fa7a5ae491a2fc71321fec
Andy Lie <andy@binus.edu>
2020-08-13 13:14:21 +0700

    renew

---
+++ precache-manifest.13c918916973d332bcf17ffde5119107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88789cd493bc4ec07bdefe1c25cc6de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e3b008ab7378b34165",
+    "url": "/static/css/main.b375fdde.chunk.css"
+  },
+  {
+    "revision": "6cfa9b13782e35cbbf9a",
+    "url": "/static/js/2.bb15699f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb15699f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e3b008ab7378b34165",
+    "url": "/static/js/main.3435a9fb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
